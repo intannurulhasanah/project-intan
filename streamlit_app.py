238040b774e0c3f79e2c7eda3e91a0a20bd0bfe9
intannurulhasanah <intannurulhasanah25@gmail.com>
2025-12-11 15:27:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("hello princess intan")
+st.title("pink:[hello princess intan]")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    ":purple[Let's start the fun game]"
 )
